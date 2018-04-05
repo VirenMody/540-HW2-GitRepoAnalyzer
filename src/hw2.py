8351a3a518a5965aa432d2f2b03db186403675e3
Viren Mody <virenmody@gmail.com>
2018-04-05 08:38:17 -0500

    iterate both lists of entities

--- src/hw2.py
+++ src/hw2.py
@@ -1,13 +1,19 @@
 import understand
 import itertools
 
+# TODO Retrieve Github pull requests and download commits
 
-# Open Database
-# db = understand.open("/home/guillermo/cs540/project.udb")
+# TODO Update the following paths
 ORIG_DB_PATH = "C:/Users/Viren/Google Drive/1.UIC/540/hw2/guillermo_rojas_hernandez_viren_mody_hw2/original.udb"
 NEW_DB_PATH = "C:/Users/Viren/Google Drive/1.UIC/540/hw2/guillermo_rojas_hernandez_viren_mody_hw2/new.udb"
 
-db_original = understand.open(ORIG_DB_PATH)
-db_new = understand.open(NEW_DB_PATH)
+# Open Database
+orig_db = understand.open(ORIG_DB_PATH)
+new_db = understand.open(NEW_DB_PATH)
 
-// Get
+# Retrieve a list of all entities
+orig_ents = orig_db.ents();
+new_ents = new_db.ents();
+
+for o_ent, n_ent in zip(orig_ents, new_ents):
+    print('{} : {}'.format(o_ent, n_ent))
