4a5dd1f304134745803c44b391ae213c716ba3a9
Viren Mody <virenmody@gmail.com>
2018-04-05 08:15:16 -0500

    Added hw2 project, updated Understand Script

--- TestUnderstand/src/Proj2.py
+++ TestUnderstand/src/Proj2.py
@@ -1,9 +1,10 @@
-#Use Understand Python API to parse through a Java file in a Java Project
+# Use Understand Python API to parse through a Java file in a Java Project
 
 import understand
+import itertools
 
 def fileCleanText(file):
-    returnString = "";
+    returnString = ""
     # Open the file lexer with macros expanded, inactive code removed, tabstop set to 8
     for lexeme in file.lexer(False,8,False,True):
         # Go through lexemes in the file and append
@@ -16,33 +17,53 @@ def fileCleanText(file):
     return returnString
 
 # Open Database
-db = understand.open("/home/guillermo/cs540/project.udb")
+# db = understand.open("/home/guillermo/cs540/project.udb")
+# db = understand.open("C:/Users/Viren/Google Drive/1.UIC/540/hw2/guillermo_rojas_hernandez_viren_mody_hw2/project.udb")
+dbOriginal = understand.open("C:/Users/Viren/Google Drive/1.UIC/540/hw2/guillermo_rojas_hernandez_viren_mody_hw2/new.udb")
+dbNew = understand.open("C:/Users/Viren/Google Drive/1.UIC/540/hw2/guillermo_rojas_hernandez_viren_mody_hw2/original.udb")
 
 # Go through files in understand
-for file in db.ents("File"):
-    #Print directory name
-    print(file.longname())
+# for file in dbOriginal.ents("File"):
+    # Print directory name
+    # print(file.longname())
 
 # Search for the first file named 'test' and print
 # the file name and the cleaned text
-file = db.lookup(".*test.*", "file")[0]
+# file = dbOriginal.lookup("Main.java", "file")[0]
 
-print(file.longname())
-print(fileCleanText(file))
+# print(file.longname())
+# print(fileCleanText(file))
 
-ents = db.ents()
+# ents = dbOriginal.ents()
+
+fileOriginal = dbOriginal.lookup("Main.java")[0]
+fileNew = dbNew.lookup("Main.java")[0]
+fileOriginal2 = dbOriginal.lookup("Main.java")[0]
+
+print(fileOriginal==fileOriginal2)
+print(fileOriginal==fileNew)
+
+print(fileOriginal.ents("Java Method"))
+
+
+#
+# for lexeme in fileOriginal.lexer():
+#   print (lexeme.text(),end="")
+#   if lexeme.ent():
+#     print ("@",end="")
 
 # print entities sorted by name of entity
-#for ent in sorted(ents, key=lambda ent: ent.longname()):
-    #print(ent.longname(), "(", ent.parameters(), ")")
+# for ent in sorted(ents, key=lambda ent: ent.longname()):
+#     print(ent.longname(), "(", ent.parameters(), ")")
 
 # print characteristics of unsorted entities
-for ent in ents:
-    print("Longname: " + ent.longname())
-    print("Parameters: ", ent.parameters())
-    print("Kind: ", ent.kind())
-    print("Metric: ", ent.metrics())
-    print("Parent: ", ent.parent())
-    print("Type: ", ent.type())
-    print("Name: ", ent.name())
-    print("---")
+# for ent in ents:
+#     print(ent.name())
+#     print("Longname: " + ent.longname())
+#     print("Parameters: ", ent.parameters())
+#     print("Kind: ", ent.kind())
+#     print("Metric: ", ent.metrics())
+#     print("Parent: ", ent.parent())
+#     print("Type: ", ent.type())
+#     print("Name: ", ent.name())
+#     print("---")
---
+++ src/hw2.py
@@ -0,0 +1,13 @@
+import understand
+import itertools
+
+
+# Open Database
+# db = understand.open("/home/guillermo/cs540/project.udb")
+ORIG_DB_PATH = "C:/Users/Viren/Google Drive/1.UIC/540/hw2/guillermo_rojas_hernandez_viren_mody_hw2/original.udb"
+NEW_DB_PATH = "C:/Users/Viren/Google Drive/1.UIC/540/hw2/guillermo_rojas_hernandez_viren_mody_hw2/new.udb"
+
+db_original = understand.open(ORIG_DB_PATH)
+db_new = understand.open(NEW_DB_PATH)
+
+// Get
