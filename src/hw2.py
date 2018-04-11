56e63a83b3a69bca6aa22feb1632e243a2f3e39f
Viren Mody <virenmody@gmail.com>
2018-04-11 16:49:47 -0500

    added checkout pr commit and parent commit

--- src/hw2.py
+++ src/hw2.py
@@ -16,8 +16,8 @@ GITHUB_USERNAME = 'virenmody'
 GITHUB_ACCESS_TOKEN = 'a74c9704e00d767da4fe1d34aaf0ed8603d8ea11'
 
 LOCAL_CLONED_REPO_PATH = 'C:/Users/Viren/Google Drive/1.UIC/540/hw2/ClonedRepos/'
-ORIG_DB_PATH = 'C:/Users/Viren/Google Drive/1.UIC/540/hw2/guillermo_rojas_hernandez_viren_mody_hw2/original.udb'
-NEW_DB_PATH = 'C:/Users/Viren/Google Drive/1.UIC/540/hw2/guillermo_rojas_hernandez_viren_mody_hw2/new.udb'
+ORIG_DB_PATH = 'C:/Users/Viren/Google Drive/1.UIC/540/original.udb'
+NEW_DB_PATH = 'C:/Users/Viren/Google Drive/1.UIC/540/new.udb'
 G_ORIG_DB_PATH = '/home/guillermo/cs540/java_project.udb'
 G_NEW_DB_PATH = '/home/guillermo/cs540/java_project2.udb'
 
@@ -30,8 +30,12 @@ git_hub = GitHub(GITHUB_USERNAME, GITHUB_ACCESS_TOKEN)
 repo_owner = 'SquareSquash'
 repo_name = 'java'
 git_url = 'https://github.com/' + repo_owner + '/' + repo_name + '.git'
-repo_dir = LOCAL_CLONED_REPO_PATH + repo_owner + '/' + repo_name
-print(repo_dir)
+repo_dir = LOCAL_CLONED_REPO_PATH + repo_owner + repo_name
+
+# Clone the repository
+print('Cloning ' + repo_owner + '/' +repo_name + ' to repo directory: ' + repo_dir)
+cloned_repo = Repo.clone_from(git_url, repo_dir)
+assert cloned_repo.__class__ is Repo  # clone an existing repository
 
 # Retrieve repository object
 test_repo = git_hub.repository(repo_owner, repo_name)
@@ -42,6 +46,8 @@ pull_requests = [pr.refresh() for pr in test_repo.pull_requests('closed', None, 
 # TODO pull correct hash for pull request commit and parent commit
 # Retrieve the commits of all pull requests that have been merged and contain only 1 commit
 commits = []
+pr_commit_hash = ''
+pr_parent_hash = ''
 for pr in pull_requests:
     if pr.merged:
         # if pr.commits_count == 1:
@@ -49,24 +55,41 @@ for pr in pull_requests:
         # print(pr.patch())
         commits = [commit.refresh() for commit in pr.commits(-1, None)]
         print('Files: ', commits[0].files)
-        print('SHA: ', commits[0].sha)
-        print('Parents: ', commits[0].parents)
+        pr_commit_hash = commits[0].sha
+        pr_parent_hash = commits[0].parents[0]['sha']
+        print('SHA: ', pr_commit_hash)
+        print('Parents HASH: ', pr_parent_hash)
+
+        # TODO organize this into functions
+        # TODO create Understand DB based on pull request commit and parent commit
+        # Checkout pull request parent commit
+        cmd = ['git', 'checkout', pr_parent_hash]
+        p = subprocess.Popen(cmd, shell=True, cwd=repo_dir, stdout=subprocess.PIPE)
+        # p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        for line in p.stdout:
+            print('LINE: ', line)
+        p.wait()
+        print('RETURN CODE: ', p.returncode)
+
+        # Checkout pull request's parent commit
+        cmd = ['git', 'checkout', pr_commit_hash]
+        p = subprocess.Popen(cmd, shell=True, cwd=repo_dir, stdout=subprocess.PIPE)
+        # p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        for line in p.stdout:
+            print('LINE: ', line)
+        p.wait()
+        print('RETURN CODE: ', p.returncode)
+
+        # Checkout master
+        cmd = ['git', 'checkout', 'master']
+        p = subprocess.Popen(cmd, shell=True, cwd=repo_dir, stdout=subprocess.PIPE)
+        # p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        for line in p.stdout:
+            print('LINE: ', line)
+        p.wait()
+        print('RETURN CODE: ', p.returncode)
 
 
-# Clone the repository
-cloned_repo = Repo.clone_from(git_url, repo_dir)
-assert cloned_repo.__class__ is Repo     # clone an existing repository
-
-sha = '5ee62f9977659e03e39406adcc4f6859e4524a0e'
-
-# TODO Checkout pull request commit and the parent commit
-cmd = ['git', 'checkout', sha]
-p = subprocess.Popen(cmd, shell=True, cwd=repo_dir, stdout=subprocess.PIPE)
-# p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-for line in p.stdout:
-    print('LINE: ', line)
-p.wait()
-print('RETURN CODE: ', p.returncode)
 
 # TODO Create and execute a shell script to create udbs with downloaded commits
 # Open Database
